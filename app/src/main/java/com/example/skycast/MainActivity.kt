183d3defdab016b841d545ea03835a5107745877
mahm0ud-hamed <mahmoudmohamedhmd@gmail.com>
2024-10-26 00:05:45 +0300

    complete five day forecast dato on gridlayout

--- app/src/main/java/com/example/skycast/MainActivity.kt
+++ app/src/main/java/com/example/skycast/MainActivity.kt
@@ -56,9 +56,44 @@ class MainActivity : AppCompatActivity() {
                     is Result.Error -> Log.i("data" , "fail")
                     Result.Loading -> Log.i("data" , "Loading")
                     is Result.Success -> it.data.collect{
+                        /*setting the max and minimum temperature in the five day forecast */
                         val dayone = getMaxAndMinTemperaturePerDay(it.list.subList(0,7))
+                        val dayTwo = getMaxAndMinTemperaturePerDay(it.list.subList(8 ,15))
+                        val daythree = getMaxAndMinTemperaturePerDay(it.list.subList(16 ,23))
+                        val dayFour = getMaxAndMinTemperaturePerDay(it.list.subList(24,31))
+                        val dayFive= getMaxAndMinTemperaturePerDay(it.list.subList(32 ,39))
+
                         binding.tvFdTodayTemp1.text= dayone.first.toString()
-                        binding.tvFdTodayTemp2.text= dayone.second.toString()
+                        binding.tvFdTodayTemp2.text= " /${dayone.second} \u2103"
+                        binding.tvFdSecondTemp1.text= dayTwo.first.toString()
+                        binding.tvFdSecondTemp2.text= " /${dayTwo.second} \u2103"
+                        binding.tvFdThirdTemp1.text= daythree.first.toString()
+                        binding.tvFdThirdTemp2.text= " /${ daythree.second } \u2103"
+                        binding.tvFdFourthTemp1.text= dayFour.first.toString()
+                        binding.tvFdFourthTemp2.text= " /${ dayFour.second } \u2103"
+                        binding.tvFdFifthTemp1.text= dayFive.first.toString()
+                        binding.tvFdFifthTemp2.text= " /${ dayFive.second } \u2103"
+
+
+                        binding.tvFdSecondDay.text = convertDateToHmanRedable(it.list.get(8).dt.toLong()).second
+                        binding.tvFdThirdDay.text = convertDateToHmanRedable(it.list.get(16).dt.toLong()).second
+                        binding.tvFdFourthDay.text = convertDateToHmanRedable(it.list.get(24).dt.toLong()).second
+                        binding.tvFdFifthDay.text = convertDateToHmanRedable(it.list.get(32).dt.toLong()).second
+
+                        setWeatherStateImage(binding.imgvFdTody,it.list.get(0).weather.get(0).icon)
+                        setWeatherStateImage(binding.imgvFdSecondDay,it.list.get(9).weather.get(0).icon)
+                        setWeatherStateImage(binding.imgvFdThirdDay,it.list.get(16).weather.get(0).icon)
+                        setWeatherStateImage(binding.imgvFdFourthDay,it.list.get(24).weather.get(0).icon)
+                        setWeatherStateImage(binding.imgvFdFifthDay,it.list.get(35).weather.get(0).icon)
+
+                        binding.tvFdTodaySkyState.text = it.list.get(0).weather[0].main
+                        binding.tvFdSecondSkyState.text = it.list.get(8).weather[0].main
+                        binding.tvFdThirdSkyState.text = it.list.get(16).weather[0].main
+                        binding.tvFdFouthSkyState.text = it.list.get(23).weather[0].main
+                        binding.tvFdFifthSkyState.text = it.list.get(32).weather[0].main
+
+
+
 
                         dayDetailsAdapter.updateList(it.list.subList(dayOneStart, dayOneEnd))
                         dayDetailsAdapter.notifyDataSetChanged()
@@ -103,7 +138,7 @@ fun getMaxAndMinTemperaturePerDay(dayInfo:kotlin.collections.List<List<Any?>>):P
 fun setCurrentDayWeatherDetalis(info : CurrentWeather , binding :ActivityMainBinding){
     setCityName(info , binding.tvCityName)
     setDate(info , binding.tvDate)
-    setWeatherStateImage(binding.imgvWeatherState,info)
+    setWeatherStateImage(binding.imgvWeatherState,info.weather.get(0).icon)
     setWeatherDescription(info , binding.tvSkyState)
     setCurrentTemperature(info ,binding.tvCurrentTemp)
     binding.tvPressureValue.text = info.main.pressure.toString()
@@ -131,9 +166,9 @@ fun convertDateToHmanRedable( unixTime : Long):Pair<String, String>{
 }
 
 
-fun setWeatherStateImage(imgv: ImageView, info: CurrentWeather){
+fun setWeatherStateImage(imgv: ImageView, info: String){
     var imgThumbnail = "https://openweathermap.org/img/wn/"
-    Glide.with(imgv.context).load("${imgThumbnail}${info.weather.get(0).icon}@2x.png").into(imgv)
+    Glide.with(imgv.context).load("${imgThumbnail}${info}@2x.png").into(imgv)
 }
 
 fun setWeatherDescription(info :CurrentWeather,tvDescription :TextView){
